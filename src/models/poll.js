4c0205bf584d130343579ed39627a916f2b694cd
Shivam Goyal <Shivam.Goyal.TH@dartmouth.edu>
2018-05-20 14:46:01 -0400

    submission

---
+++ src/models/poll.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PollSchema = new Schema({
+  text: String,
+  imageURL: String,
+  upvotes: { type: Number, default: 0 },
+  downvotes: { type: Number, default: 0 },
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+});
+
+PollSchema.virtual('score').get(function scoreCalc() {
+  return this.upvotes - this.downvotes;
+});
+
+// create model class
+const PollModel = mongoose.model('Poll', PollSchema);
+
+export const getPolls = () => {
+  return Poll.find({});
+};
+
+export default PollModel;
+
